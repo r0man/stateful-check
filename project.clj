a660ebe5117ffd74287fabd791f9312cbb685564
Roman Scherer <roman@burningswell.com>
2023-05-27 08:48:26 +0200

    Add test.chuck and bump test.check

--- project.clj
+++ project.clj
@@ -3,8 +3,9 @@
   :url "https://github.com/czan/stateful-check"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/test.check "0.9.0"]]
+  :dependencies [[com.gfredericks/test.chuck "0.2.14"]
+                 [org.clojure/clojure "1.8.0"]
+                 [org.clojure/test.check "1.1.1"]]
   :test-selectors {:default #(not (:interactive %))
                    :interactive :interactive}
   :repositories [["releases" {:url "https://clojars.org/repo"
